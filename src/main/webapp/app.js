0637baa7e9d454bd0a34896128fbac293c883573
Joachim <Joachim@10.50.138.99>
2016-10-14 11:27:09 +0200

    Fået alt bortset fra detail siden til at fungere.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,45 +1,47 @@
-(function(){
+(function () {
     var app = angular.module("routingPersons", ["ngRoute"]);
-    app.config(function($routeProvider) {
+    app.config(function ($routeProvider) {
         $routeProvider
-        .when("/all", {
-            templateUrl: "allPersons.html",
-            controller: "personController as personCtrl"
-        })
-        .when("/new", {
-            templateUrl: "newPerson.html",
-            controller: "personController as personCtrl"
-        })
-        .when("/detail", {
-            templateUrl: "personDetails.html",
-            controller: "personController as personCtrl"
-        })
-        .otherwise({
-            template : "<p>Nothing has been selected,</p>"
-        });
+                .when("/all", {
+                    templateUrl: "allPersons.html",
+                    controller: function () {
+                        this.persons = persons;
+                        this.personDetail = function(personId){
+                            detailId = personId;
+                        };
+                    },
+                    controllerAs: "personCtrl"
+                })
+                .when("/new", {
+                    templateUrl: "newPerson.html",
+                    controller: function() {
+                        this.persons = persons;
+                        addedPerson = {};
+                        this.addPerson = function(person){
+                            addedPerson = person;
+                            addedPerson.id = persons.length + 1;
+                            persons.push(addedPerson);
+                            addedPerson = {};
+                        };                        
+                    },
+                    controllerAs: "personCtrl"
+                })
+                .when("/detail", {
+                    templateUrl: "personDetails.html",
+                    controller: function() {
+                        this.id = detailId;
+                        this.persons = persons;          
+                    },
+                    controllerAs: "personCtrl"
+                })
+                .otherwise({
+                    template: "<p>Nothing has been selected,</p>"
+                });
     });
-    var detailId = 0;
     var persons = [
-        {id: 1,name: "Jens",age : 18}
-        , {id: 2,name: "Peter",age : 23}
-        , {id: 3,name: "Hanne",age : 23}
+        {id: 1, name: "Jens", age: 18}
+        , {id: 2, name: "Peter", age: 23}
+        , {id: 3, name: "Hanne", age: 23}
     ];
-    app.controller("personCtrl", function() {
-        this.person = persons;
-        addedPerson = {};
-        this.personDetail = function(personId){
-            detailId = personId;
-        };
-        this.getDetails = function(){
-            return detailId;
-        };
-        this.addPerson = function(person){
-            addedPerson = person;
-            addedPerson.id = persons.length() + 1;
-            persons.push(addedPerson);
-            addedPerson = {};
-        };
-    });
-    
-    
+    var detailId = 0;
 })();
